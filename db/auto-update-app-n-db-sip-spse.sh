a4962c983ec3ecad60491f2be8002cb2cc2440b0
Danang Ponorogo <danang.dandies@gmail.com>
2020-02-18 10:38:37 +0700

    Update skrip restore app n db spse

--- db/auto-update-app-n-db-sip-spse.sh
+++ db/auto-update-app-n-db-sip-spse.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# 0 1 * * * /root/auto-update-app-n-db-sip-spse.sh
+# 0 1 * * * /var/www/html/sip-spse/db/auto-update-app-n-db-sip-spse.sh
 
-pg_restore -vc --if-exists -U postgres -j 8 -d epns_prod '/home/andi/epns_prod.backup'
-psql -U postgres -d epns_prod -f /root/recreate-view-sip.sql
 cd /var/www/html/sip-spse/
 git pull
+
+tgl=$(date "+%d-%m-%Y")
+
+psql -U postgres -d epns_prod -f '/var/www/html/sip-spse/db/reset_schema_public_n_ekontrak.sql'
+gzip -d '/home/backupdb/epns_prod_'"$tgl"'.backup.gz'
+pg_restore -vc --if-exists -U postgres -j 8 -d epns_prod '/home/backupdb/epns_prod_'"$tgl"'.backup'
+psql -U postgres -d epns_prod -f '/var/www/html/sip-spse/db/recreate-view-sip.sql'
--- db/update_sip-spse.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /var/www/html/sip-spse/
-git pull
