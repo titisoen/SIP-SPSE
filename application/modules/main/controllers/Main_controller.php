cc7071c42f6e79e627c197319da0f6057c3b57af
Danang Ponorogo <danang.dandies@gmail.com>
2020-02-22 21:00:48 +0700

    Tambah fitur info build

--- application/modules/main/controllers/Main_controller.php
+++ application/modules/main/controllers/Main_controller.php
@@ -51,6 +51,32 @@ class Main_controller extends CI_Controller {
 		$data["misc"] 	= array("nama_kota" => $nama_kota, "nama_admin" => $nama_admin, "url_admin" => $url_admin);
 		$data['kode_klpd'] = $this->model->get_info_klpd()->row()->kode_klpd;
 		$data['klpd'] = $this->model->get_info_klpd()->row()->nama_klpd;
+		$data['build'] = $this->get_build();
 		$this->load->view('Main_view', $data);
+	}
+	
+	public function get_build(){
+		$date = new DateTime();
+
+		$fn = fopen(".git/logs/HEAD","r");
+		
+		$build = '';
+		
+		while(! feof($fn))
+		{
+			$result = fgets($fn);
+			if (count(explode('+0700', $result))>1)
+			{			
+				list($a, $b) = explode('+0700', $result);
+				$arr = explode(' ', trim($a));
+				$timestamp = end($arr);
+				$date->setTimestamp($timestamp);
+				//echo 'Build: ' . $date->format('d-m-Y H:i:s') . ' # '. trim(str_replace('commit:', '', $b));
+				$build = $date->format('d-m-Y H:i:s');
+			}
+		}
+		fclose($fn);
+		
+		return 'Build: ' . $build;
 	}
 }
--- application/modules/main/views/default/footer.php
+++ application/modules/main/views/default/footer.php
@@ -1,4 +1,5 @@
 <div class="drawer-footer" align="center">
 	<p class="copyright">2019&nbsp;&copy;&nbsp;<a href="http://lpse.ponorogo.go.id/eproc4/publik/kontakkami" target="_blank" rel="nofollow">Komunitas LPSE</a>
 	</p>
+	<p class="copyright"><?=$build;?></p>
 </div>
