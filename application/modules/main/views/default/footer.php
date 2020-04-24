514393b0f2887c0a61483f009c7aef53578e951f
Danang Ponorogo <danang.dandies@gmail.com>
2020-04-24 07:03:03 +0700

    Tambah pengembang dan email komunitas

--- application/modules/main/views/default/footer.php
+++ application/modules/main/views/default/footer.php
@@ -1,5 +1,5 @@
 <div class="drawer-footer" align="center">
-	<p class="copyright">2019&nbsp;&copy;&nbsp;<a href="http://lpse.ponorogo.go.id/eproc4/publik/kontakkami" target="_blank" rel="nofollow">Komunitas LPSE</a>
+	<p class="copyright">2019&nbsp;&copy;&nbsp;<a href="javascript:void(0)" class="open-misc-tentangkami" rel="nofollow">Komunitas LPSE</a>
 	</p>
 	<p class="copyright"><?=$build;?></p>
 </div>
